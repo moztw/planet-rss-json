7b2f7ba5e60a1ff2b4813c2e323627ae4647fab8
Ming Tsay <mt@mingtsay.tw>
2022-05-25 12:26:43 +0800

    Refactor script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const { stdout } = require('node:process')
-const { parse } = require('rss-to-json')
-
 const rssUrl = 'https://planet.moztw.org/atom.xml'
 
-parse(rssUrl)
-  .then(rss => JSON.stringify(rss))
-  .then(json => stdout.write(json))
+require('rss-to-json')
+  .parse(rssUrl)
+  .then(rss => {
+    const json = JSON.stringify(rss)
+    process.stdout.write(json)
+  })
